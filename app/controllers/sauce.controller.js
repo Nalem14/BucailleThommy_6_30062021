022a9494ba9a3f90f60bf34291230265fc418386
Thommy Bucaille <nalem.14360@gmail.com>
2021-07-08 12:05:58 +0200

    Add error when no sauces

--- app/controllers/sauce.controller.js
+++ app/controllers/sauce.controller.js
@@ -4,6 +4,10 @@ const Sauce = db.sauce;
 // List sauces
 exports.list = (req, res) => {
     Sauce.find().then(datas => {
+        if(datas.length == 0) {
+            return res.status(401).json({ error: "Aucune souce n'est disponible" });
+        }
+
         res.status(200).json(datas);
     })
 };
