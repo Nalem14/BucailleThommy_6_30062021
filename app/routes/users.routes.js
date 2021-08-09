305014ebd889f7da30f78658e3ad215b16742fdf
Thommy Bucaille <nalem.14360@gmail.com>
2021-08-09 13:26:19 +0200

    Fixe

--- app/routes/users.routes.js
+++ app/routes/users.routes.js
@@ -7,6 +7,6 @@ router.post("/signup", userController.signup);
 router.post("/login", userController.login);
 router.get("/get-datas", authMiddleware, userController.getDatas);
 router.get("/export-datas", authMiddleware, userController.exportDatas);
-router.post("/delete", authMiddleware, userController.delete);
+router.delete("/delete", authMiddleware, userController.delete);
 
 module.exports = router;
