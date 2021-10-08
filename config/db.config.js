1a2ae08c0240b3e8bf9e964231a0db7decf5cea6
Thommy Bucaille <nalem.14360@gmail.com>
2021-10-08 10:36:45 +0200

    logs debug mongoose

--- config/db.config.js
+++ config/db.config.js
@@ -2,6 +2,9 @@ const mongoose = require("mongoose");
 const bunyan = require("bunyan");
 const log = bunyan.createLogger({
   name: "MongoDB Driver",
+  serializers: {
+    dbQuery: serializer,
+  },
   streams: [
     {
       stream: process.stdout,
@@ -29,12 +32,31 @@ if (!process.env.DB_URL) {
   console.log("No DB_URL found in .env configuration");
 }
 
+function serializer(data) {
+  let query = JSON.stringify(data.query);
+  let options = JSON.stringify(data.options || {});
+
+  return `db.${data.coll}.${data.method}(${query}, ${options});`;
+}
+
+mongoose.set('debug', function(coll, method, query, doc, options) {
+  let set = {
+      coll: coll,
+      method: method,
+      query: query,
+      doc: doc,
+      options: options
+  };
+
+  log.info({
+      dbQuery: set
+  });
+});
+
 mongoose
   .connect(process.env.DB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    logger: console.log,
-    loggerLevel: 'info',
   })
   .then(() => {
     console.log("Connected to database");
